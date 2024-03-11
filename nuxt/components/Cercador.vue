8dc4330b67f3b1eb8c0453c40fd0ee95a780f9f8
Àlvaro Kumenius <akumenius@gmail.com>
2024-03-11 12:39:15 +0100

    Commit

---
+++ nuxt/components/Cercador.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+    <div class="cercador">Buscar...</div>
+</template>
+
+<style scoped>
+
+.cercador {
+    background-color: white;
+    border-radius: 24px;
+    width: 80%;
+    height: 40px;
+}
+    
+</style>
