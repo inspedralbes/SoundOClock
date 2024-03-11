d3a11e04cff0f02700d7f7d32e88abb7a82b540a
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-03-11 09:52:21 +0100

    Update docker-compose.yml and DatabaseSeeder.php, add seed data

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -4,8 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Eloquent;
-use DB;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class DatabaseSeeder extends Seeder
 {
