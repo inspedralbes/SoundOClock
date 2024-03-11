7408209de4a8e795ebdb6a80d5f3f32ae4fcc138
mm333mm <a20miqmorcho@inspedralbes.cat>
2024-03-11 13:22:22 +0100

    crear llistat en json

--- nuxt/pages/escollirC.vue
+++ nuxt/pages/escollirC.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
+
         <h1>Escollir C</h1>
+        <div v-for="song in llistat" :key="song.id">
+            <h2>{{ song.title }}</h2>
+            <p>{{ song.artist }}</p>
+            <p>{{ song.year }}</p>
+        </div>
+        
     </div>
 </template>
 
@@ -8,13 +15,16 @@
 export default {
     data() {
         return {
-            escollirC: []
+            llistat: []
         }
     },
     mounted() {
-        fetch('http://localhost:3000/api/escollirC')
+        fetch('/llistatM.json')
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => {
+                this.llistat = data
+                console.log(this.llistat);
+            })
     },
     methods: {
 
