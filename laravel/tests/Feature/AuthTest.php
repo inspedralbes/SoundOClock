791a6f577b8b9e9d480a6bd7afa8caced8a5fbed
santipu03 <santipujolsau@gmail.com>
2024-03-20 12:59:46 +0100

    Create tests for Auth and Blacklist

---
+++ laravel/tests/Feature/AuthTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase {
+
+    use RefreshDatabase;
+
+    public function test_login(): void {
+        // Login
+        $response = $this->post('/api/login', [
+            'email' => 'santi@gmail.com',
+            'name' => 'Santi'  
+        ]);
+
+        // Check if the response is correct
+        $response->assertStatus(201)
+            ->assertJsonStructure([
+                'user' => [
+                    'id',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                ],
+                'token'
+            ]);
+    }
+
+    public function test_logout(): void {
+        // Login first
+        $response = $this->post('/api/login', [
+            'email' => 'santi@gmail.com',
+            'name' => 'Santi'
+        ]);
+        $token = $response->json('token');
+
+        // Logout
+        $response = $this->withHeader('Authorization', "Bearer $token")
+            ->post('/api/logout');
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'message' => 'Logged out'
+            ]);
+    }
+
+    public function test_getUser(): void {
+        // Login first
+        $response = $this->post('/api/login', [
+            'email' => 'santi@gmail.com',
+            'name' => 'Santi'
+        ]);
+        $token = $response->json('token');
+
+        // Get user
+        $response = $this->withHeader('Authorization', "Bearer $token")
+            ->post('/api/getUser');
+        
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at',
+                'is_admin',
+                'class_group_id'
+            ]);
+    }
+}
