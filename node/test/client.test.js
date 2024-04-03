00c3dc08048efb5ca174dee7c65ad755f4344bcc
Pedro Garcia <a20pedgarguz@inspedralbes.cat>
2024-04-03 09:19:31 +0200

    Add mongoose connection and update tests

--- node/test/client.test.js
+++ node/test/client.test.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import { io as ioClient } from 'socket.io-client';
 import { Song, VotingRecord, ReportSong } from '../models.js';
+import mongoose from 'mongoose';
 import comManager from '../communicationManager.js';
-import { mongoose, connectToDatabase } from '../moongoseConfig.js';
 
 describe('Listen the Server sockets', function () {
   let clientSocket;
@@ -27,7 +27,7 @@ describe('Listen the Server sockets', function () {
     clientSocket = ioClient(serverAddr);
     await new Promise((resolve) => clientSocket.on('connect', resolve));
     // Mongoose setup for accesing directly to the database
-    await connectToDatabase(process.env.MONGO_HOST)
+    mongoose.connect('mongodb://mongoadmin:mongopassword@127.0.0.1:27017/soundoclock', { authSource: "admin" })
       .then(() => console.log('MongoDB connected'))
       .catch(err => console.error('MongoDB connection error:', err));
     // Logins to get the tokens
