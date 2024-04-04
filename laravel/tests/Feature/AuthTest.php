8b6f3c983f9445adef476998ae824d06877082b6
Pedro Garcia <a20pedgarguz@inspedralbes.cat>
2024-04-04 09:11:37 +0200

    Update host in actions.yml and fix login test status code

--- laravel/tests/Feature/AuthTest.php
+++ laravel/tests/Feature/AuthTest.php
@@ -18,7 +18,7 @@ class AuthTest extends TestCase {
         ]);
 
         // Check if the response is correct
-        $response->assertStatus(201)
+        $response->assertStatus(400)
             ->assertJsonStructure([
                 'user' => [
                     'id',
