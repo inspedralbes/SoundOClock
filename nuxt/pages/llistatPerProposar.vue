a5113f0ece86ec4e345f2a4519ce6d82924795be
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-04-11 10:10:15 +0200

    Refactor component rendering and cleanup in llista_propostes.vue and llistatPerProposar.vue

--- nuxt/pages/llistatPerProposar.vue
+++ nuxt/pages/llistatPerProposar.vue
@@ -272,7 +272,9 @@ export default {
             this.currentTrack.pause();
         }
     },
-    beforeDestroy() {
+    beforeUnmount() {
+        this.store.deleteCurrentTrackPlaying(null);
+        this.currentTrack.pause();
         socket.off('topSongs');
         socket.off('searchResult');
         socket.off('sendHtmlSpotify');
