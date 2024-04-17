1fe4b94ff2eaf146b488ffa12224dd4f32ebee77
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-04-17 08:58:55 +0200

    Update production API URLs in .env.example, communicationManager.js, and socket.js

--- nuxt/communicationManager.js
+++ nuxt/communicationManager.js
@@ -4,7 +4,7 @@ import { useAppStore } from './stores/app.js';
 // const url = "http://localhost:8080";
 
 // PRODUCCIÓN
-const url = "http://129.151.244.179:8080";
+const url = "http://143.47.53.38:8080";
 
 function getUserSelectedSongs(id) {
     const store = useAppStore();
