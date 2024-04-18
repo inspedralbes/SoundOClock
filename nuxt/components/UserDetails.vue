d27ca60c9169cd74985044ffcf871dc4b6f22829
Dasbits <a20davsalsos@inspedralbes.cat>
2024-04-18 10:33:21 +0200

    defaultOpt fet - posar be el recuadre de filtres

--- nuxt/components/UserDetails.vue
+++ nuxt/components/UserDetails.vue
@@ -14,6 +14,8 @@ export default {
       proposingBannedUntil: null,
       weeksVotingBannedUntil: null,
       weeksProposingBannedUntil: null,
+      optionVotingBannedUntil: null,
+      optionProposingBannedUntil: null,
       toggleVotingBanUserWeeksOrCustomize: true,
       toggleProposingBanUserWeeksOrCustomize: true,
       modals: {
@@ -22,8 +24,8 @@ export default {
         unbanUserVotingCapacity: false,
         banUserProposingCapacity: false,
         unbanUserProposingCapacity: false,
-        banUserVotingForWeek: false,
-        banUserProposingForWeek: false
+        banUserVotingWithDefaultOptions: false,
+        banUserProposingWithDefaultOptions: false
       }
     }
   },
@@ -87,14 +89,64 @@ export default {
 
       return `${year}-${month}-${day}`;
     },
-    banUserVotingForWeek(weeks) {
-      console.log("banUserVotingForWeek", weeks)
-
+    getBanDate(option) {
+      let date
+      switch (option) {
+        case 1: // 3 weeks
+          date = new Date(Date.now() + 3 * 7 * 24 * 60 * 60 * 1000).toLocaleDateString('es-ES').replace(/\//g, '-')
+          break;
+        case 2: // 3 motnhs
+          date = new Date(new Date().setMonth(new Date().getMonth() + 3)).toLocaleDateString('es-ES').replace(/\//g, '-')
+          break;
+        case 3: // 1 year
+          date = new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toLocaleDateString('es-ES').replace(/\//g, '-')
+          break;
+      }
+      return date
     },
-    banUserProposingForWeek(weeks) {
-      console.log("banUserProposingForWeek", weeks)
-
+    banUserVotingWithDefaultOptions(option) {
+      let date
+      switch (option) {
+        case 1: // 3 weeks
+          date = new Date(Date.now() + 3 * 7 * 24 * 60 * 60 * 1000).toISOString().split('T')[0].replace(/-/g, '-');
+          break;
+        case 2: // 3 motnhs
+          date = new Date(new Date().setMonth(new Date().getMonth() + 3)).toISOString().split('T')[0].replace(/-/g, '-');
+          break;
+        case 3: // 1 year
+          date = new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString().split('T')[0].replace(/-/g, '-');
+          break;
+      }
+      this.user.vote_banned_until = date;
+      socket.emit('banUser', this.store.getUser().token, this.user);
     },
+    banUserProposingWithDefaultOptions(option) {
+      let date
+      switch (option) {
+        case 1: // 3 weeks
+          date = new Date(Date.now() + 3 * 7 * 24 * 60 * 60 * 1000).toISOString().split('T')[0].replace(/-/g, '-');
+          break;
+        case 2: // 3 motnhs
+          date = new Date(new Date().setMonth(new Date().getMonth() + 3)).toISOString().split('T')[0].replace(/-/g, '-');
+          break;
+        case 3: // 1 year
+          date = new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString().split('T')[0].replace(/-/g, '-');
+          break;
+      }
+      this.user.propose_banned_until = date;
+      socket.emit('banUser', this.store.getUser().token, this.user);
+    },
+    enableUserWithDefaultOptions(option) {
+      switch (option) {
+        case 1: // enable voting
+          this.user.vote_banned_until = null;
+          break;
+        case 2: // enable proposing
+          this.user.propose_banned_until = null;
+          break;
+      }
+      socket.emit('banUser', this.store.getUser().token, this.user);
+    }
   },
   setup() {
     const store = useAppStore();
@@ -118,30 +170,33 @@ export default {
         <p v-else class="mb-2 text-xl text-center font-black">L'usuari no té limitada la capacitat de votar cançons</p>
         <h2 class="text-2xl mb-4 text-center">LIMITAR VOTAR CANÇONS</h2>
         <div v-if="toggleVotingBanUserWeeksOrCustomize">
-          <div class="flex justify-center mt-4">
-            <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded me-2"
-              @click="modals.banUserVotingForWeek = true; weeksVotingBannedUntil = 1">Ban 1 setmana</button>
-            <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded me-2"
-              @click="modals.banUserVotingForWeek = true; weeksVotingBannedUntil = 2">Ban 2 setmanes</button>
-            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded me-2"
-              @click="modals.banUserVotingForWeek = true; weeksVotingBannedUntil = 3">Ban 3 setmanes</button>
+          <div v-if="this.user.vote_banned_until">
+            <button class="w-fit bg-gray-400 hover:bg-gray-200 text-black font-bold py-2 px-4 rounded"
+              @click="enableUserWithDefaultOptions(1)">HABILITAR VOTACIONS</button>
           </div>
-          <div>
-            <button @click="toggleVotingBanUserWeeksOrCustomize = false"
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded me-2 mt-5">Personalitzat</button>
+          <div v-else>
+            <div class="flex justify-center mt-4">
+              <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-3 px-4 rounded me-2 w-full"
+                @click="modals.banUserVotingWithDefaultOptions = true; optionVotingBannedUntil = 1">Ban 3
+                setmanes</button>
+              <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-3 px-4 rounded me-2 w-full"
+                @click="modals.banUserVotingWithDefaultOptions = true; optionVotingBannedUntil = 2">Ban 3 mesos</button>
+              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-4 rounded me-2 w-full"
+                @click="modals.banUserVotingWithDefaultOptions = true; optionVotingBannedUntil = 3">Ban 1 any</button>
+            </div>
+            <div>
+              <button @click="toggleVotingBanUserWeeksOrCustomize = false"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded me-2 mt-5">Personalitzat</button>
+            </div>
           </div>
         </div>
         <div v-else>
-          <Calendar class="mb-8" v-bind:date="user.vote_banned_until" :isVotingBannedDate=true
+          <Calendar class="mb-4" v-bind:date="user.vote_banned_until" :isVotingBannedDate=true
             @changeDate="changeDate" />
           <button class="w-fit bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded me-2"
             @click="banUser(true)">LIMITAR VOTACIONS</button>
-          <button class="w-fit bg-gray-400 hover:bg-gray-200 text-black font-bold py-2 px-4 rounded"
-            @click="enableUser(true)">HABILITAR VOTACIONS</button>
-          <div>
-            <button class="w-fit bg-red-400 hover:bg-red-200 text-black font-bold py-2 px-4 rounded mt-4"
-              @click="toggleVotingBanUserWeeksOrCustomize = true">Cancela</button>
-          </div>
+          <button class="w-fit bg-red-400 hover:bg-red-200 text-black font-bold py-2 px-4 rounded float-right"
+            @click="toggleVotingBanUserWeeksOrCustomize = true">Cancela</button>
         </div>
       </div>
       <div class="w-1/2">
@@ -153,31 +208,33 @@ export default {
         <h2 class="text-2xl mb-4 text-center">LIMITAR PROPOSAR CANÇONS</h2>
 
         <div v-if="toggleProposingBanUserWeeksOrCustomize">
-          <div class="flex justify-center mt-4">
-            <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded me-2"
-              @click="modals.banUserProposingForWeek = 1; weeksProposingBannedUntil = 1">Ban 1 setmana</button>
-            <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded me-2"
-              @click="modals.banUserProposingForWeek = 1; weeksProposingBannedUntil = 2">Ban 2 setmanes</button>
-            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded me-2"
-              @click="modals.banUserProposingForWeek = 1; weeksProposingBannedUntil = 3">Ban 3 setmanes</button>
+          <div v-if="this.user.propose_banned_until">
+            <button class="w-fit bg-gray-400 hover:bg-gray-200 text-black font-bold py-2 px-4 rounded"
+              @click="enableUserWithDefaultOptions(1)">HABILITAR PROPOSTES</button>
           </div>
-          <div>
-            <button @click="toggleProposingBanUserWeeksOrCustomize = false"
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded me-2 mt-5">Personalitzat</button>
+          <div v-else>
+            <div class="flex justify-center mt-4">
+              <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-3 px-4 rounded me-2 w-full"
+                @click="modals.banUserProposingWithDefaultOptions = 1; optionProposingBannedUntil = 1">Ban 3 setmanes</button>
+              <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-3 px-4 rounded me-2 w-full"
+                @click="modals.banUserProposingWithDefaultOptions = 1; optionProposingBannedUntil = 2">Ban 3 mesos</button>
+              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-4 rounded me-2 w-full"
+                @click="modals.banUserProposingWithDefaultOptions = 1; optionProposingBannedUntil = 3">Ban 1 any</button>
+            </div>
+            <div>
+              <button @click="toggleProposingBanUserWeeksOrCustomize = false"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded me-2 mt-5">Personalitzat</button>
+            </div>
           </div>
         </div>
 
         <div v-else>
-          <Calendar class="mb-8" v-bind:date="user.propose_banned_until" :isVotingBannedDate=false
+          <Calendar class="mb-4" v-bind:date="user.propose_banned_until" :isVotingBannedDate=false
             @changeDate="changeDate" />
           <button class="w-fit bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded me-2"
             @click="banUser(false)">LIMITAR PROPOSTES</button>
-          <button class="w-fit bg-gray-400 hover:bg-gray-200 text-black font-bold py-2 px-4 rounded"
-            @click="enableUser(false)">HABILITAR PROPOSTES</button>
-          <div>
-            <button class="w-fit bg-red-400 hover:bg-red-200 text-black font-bold py-2 px-4 rounded mt-4"
-              @click="toggleProposingBanUserWeeksOrCustomize = true">Cancela</button>
-          </div>
+          <button class="w-fit bg-red-400 hover:bg-red-200 text-black font-bold py-2 px-4 rounded float-right"
+            @click="toggleProposingBanUserWeeksOrCustomize = true">Cancela</button>
         </div>
 
       </div>
@@ -238,33 +295,39 @@ export default {
     </template>
   </ModularModal>
 
-  <ModularModal :open="modals.banUserVotingForWeek" type="error" msg="Limitar" title="Limitar propostes usuari"
-    @confirm="banUserVotingForWeek(weeksVotingBannedUntil)"
-    @close="modals.banUserVotingForWeek = false; weeksVotingBannedUntil = null">
+  <!-- modal per banejar les votacions -->
+  <ModularModal :open="modals.banUserVotingWithDefaultOptions" type="error" msg="Limitar"
+    title="Limitar propostes usuari" @confirm="banUserVotingWithDefaultOptions(optionVotingBannedUntil)"
+    @close="modals.banUserVotingWithDefaultOptions = false; optionVotingBannedUntil = null">
     <template #title>
-      <h2>Limitar capacitat de votar</h2>
+      <h2>Limitar capacitat de votar {{ optionVotingBannedUntil === 1 ? '3 setmanes' : optionVotingBannedUntil === 2 ?
+        '3 mesos' : '1 any' }}</h2>
     </template>
     <template #content>
       <p>Segur que vols que l'usuari <span class="font-bold">{{ user.name }}</span> no pugui votar cançons fins el
-        <span class="font-bold">{{ weeksVotingBannedUntil }}?</span>
+        <span class="font-bold">{{ getBanDate(optionVotingBannedUntil) }}?</span>
 
       </p>
     </template>
   </ModularModal>
 
-  <!-- modal per banejar per setmanes les propostes -->
-  <ModularModal :open="modals.banUserProposingForWeek" type="error" msg="Limitar" title="Limitar propostes usuari"
-    @confirm="banUserProposingForWeek(weeksProposingBannedUntil)"
-    @close="modals.banUserProposingForWeek = false; weeksProposingBannedUntil = null">
+  <!-- modal per banejar les propostes -->
+  <ModularModal :open="modals.banUserProposingWithDefaultOptions" type="error" msg="Limitar"
+    title="Limitar propostes usuari" @confirm="banUserProposingWithDefaultOptions(optionProposingBannedUntil)"
+    @close="modals.banUserProposingWithDefaultOptions = false; optionProposingBannedUntil = null">
     <template #title>
-      <h2>Limitar capacitat de proposar</h2>
+      <h2>Limitar capacitat de proposar {{ optionProposingBannedUntil === 1 ? '3 setmanes' : optionProposingBannedUntil
+        ===
+        2 ? '3 mesos' : '1 any' }}</h2>
     </template>
     <template #content>
-      <p>Segur que vols que l'usuari <span class="font-bold">{{ user.name }}</span> no pugui proposar cançons fins el
-        <span class="font-bold">{{ weeksProposingBannedUntil }}?</span>
+      <p>Segur que vols que l'usuari <span class="font-bold">{{ user.name }}</span> no pugui votar cançons fins el
+        <span class="font-bold">{{ getBanDate(optionProposingBannedUntil) }}?</span>
+
       </p>
     </template>
   </ModularModal>
+
 
 
 </template>
