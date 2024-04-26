a4123b7569ba2fc1eca55a94036d366e002d4d5d
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-04-26 10:04:01 +0200

    Update deployment run name in actions.yml and fix API URL in communicationManager.js

--- node/communicationManager.js
+++ node/communicationManager.js
@@ -49,7 +49,7 @@ async function loginUserAndAdmin() {
 }
 
 async function login(name, email) {
-  const response = await fetch(apiURL + 'login', {
+  const response = await fetch(apiURL + 'login/', {
     method: 'POST',
     headers: {
       "Content-Type": "application/json",
