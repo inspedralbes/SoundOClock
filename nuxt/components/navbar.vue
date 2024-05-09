a7f9df7af1dacfef19ae60c1d4c6d59387ce1fa3
Alvaro <akumenius@gmail.com>
2024-05-09 10:29:31 +0200

    merge conflicts

--- nuxt/components/navbar.vue
+++ nuxt/components/navbar.vue
@@ -1,26 +1,32 @@
 <template>
     <div class="h-fit flex flex-col text-lg divide-y-2 border-y-2 border-white text-[1.15rem]">
+        <NuxtLink v-if="user.role_id <= 3" class="px-4 py-4 flex align-center gap-3" to="/admin" @click="close()">
+            <span class="material-symbols-outlined text-[2rem]">
+                admin_panel_settings
+            </span>
+            <span>Administració</span>
+        </NuxtLink>
         <NuxtLink class="px-4 py-4 flex align-center gap-3" to="/llista_propostes" @click="close()">
-            <UIcon name="i-heroicons-musical-note-16-solid text-[28px]" />
+            <UIcon name="i-heroicons-musical-note-16-solid text-[2rem]" />
             <span>Votacions</span>
         </NuxtLink>
         <NuxtLink class="px-4 py-4 flex align-center gap-3" to="/els_meus_grups" @click="close()">
-            <UIcon name="i-heroicons-user-group-16-solid text-[28px]" />
+            <UIcon name="i-heroicons-user-group-16-solid text-[2rem]" />
             <span>Els meus grups</span>
         </NuxtLink>
         <NuxtLink class="px-4 py-4 flex align-center gap-3" to="/ranking" @click="close()">
-            <UIcon name="i-heroicons-hand-thumb-up-16-solid text-[28px]" />
-            Ranking provisional
+            <UIcon name="i-heroicons-hand-thumb-up-16-solid text-[2rem]" />
+            <span>Ranking provisional</span>
         </NuxtLink>
         <NuxtLink class="px-4 py-4 flex align-center gap-3" to="https://forms.gle/qKtFKNCgtQjXmpyNA">
-            <UIcon name="i-heroicons-speaker-wave-20-solid text-[28px]" />
-            Feedback
+            <UIcon name="i-heroicons-speaker-wave-20-solid text-[2rem]" />
+            <span>Feedback</span>
         </NuxtLink>
         <NuxtLink class="px-4 py-4 flex align-center gap-3" to="/logout" @click="close()">
-            <span class="material-symbols-outlined text-[28px]">
+            <span class="material-symbols-outlined text-[2rem]">
                 logout
             </span>
-            Tancar sessió
+            <span>Tancar sessió</span>
         </NuxtLink>
     </div>
 </template>
@@ -31,6 +37,7 @@ import { useAppStore } from '@/stores/app';
 export default {
     data() {
         return {
+            user: computed(() => this.store.getUser()),
         }
     },
     methods: {
