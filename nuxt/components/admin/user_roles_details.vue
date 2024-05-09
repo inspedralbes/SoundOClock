291d5c9a8d5dabcda3bbf3c39b26c1cc5f86171f
Alvaro <akumenius@gmail.com>
2024-05-09 09:53:53 +0200

    mobile-navbar

--- nuxt/components/admin/user_roles_details.vue
+++ nuxt/components/admin/user_roles_details.vue
@@ -6,8 +6,8 @@
             <p class="text-5xl font-black">{{ user.name }}</p>
         </div>
         <div class="flex flex-col gap-3">
-            <div v-for="role in roles" class="radio-card p-4 rounded-lg text-left text-xl"
-                :class="backgroundColor(role.id)">
+            <div v-for="role in roles">
+                <div v-if="role.id !=1" class="radio-card p-4 rounded-lg text-left text-xl" :class="backgroundColor(role.id)">
                 <input type="radio" :id="'option-' + role.id" name="role-options" :value="role.id" v-model="selectedRole"
                     @change="" />
                 <label :for="'option-' + role.id" class="flex flex-row items-center gap-4">
@@ -17,6 +17,7 @@
                         <p>{{ role.description }}</p>
                     </div>
                 </label>
+            </div>
             </div>
         </div>
         <button class="pedralbes-button w-fit text-white font-bold mt-4 py-2 px-4 rounded" @click="saveUserRole()">DESAR
@@ -71,16 +72,16 @@ export default {
             }
 
             switch (roleId) {
-                case 1:
+                case 2:
                     style = "bg-indigo-300";
                     break;
-                case 2:
+                case 3:
                     style = "bg-orange-300";
                     break;
-                case 3:
+                case 4:
                     style = "bg-rose-300";
                     break;
-                case 4:
+                case 5:
                     style = "bg-sky-300";
                     break;
                 default:
@@ -93,16 +94,16 @@ export default {
             let icon;
 
             switch (roleId) {
-                case 1:
+                case 2:
                     icon = "<svg  xmlns='http://www.w3.org/2000/svg'  width='48'  height='48'  viewBox='0 0 24 24'  fill='none'  stroke='currentColor'  stroke-width='2'  stroke-linecap='round'  stroke-linejoin='round'  class='icon icon-tabler icons-tabler-outline icon-tabler-settings'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z' /><path d='M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0' /></svg>";
                     break;
-                case 2:
+                case 3:
                     icon = "<svg  xmlns='http://www.w3.org/2000/svg'  width='48'  height='48'  viewBox='0 0 24 24'  fill='currentColor'  class='icon icon-tabler icons-tabler-filled icon-tabler-shield-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M11.998 2l.118 .007l.059 .008l.061 .013l.111 .034a.993 .993 0 0 1 .217 .112l.104 .082l.255 .218a11 11 0 0 0 7.189 2.537l.342 -.01a1 1 0 0 1 1.005 .717a13 13 0 0 1 -9.208 16.25a1 1 0 0 1 -.502 0a13 13 0 0 1 -9.209 -16.25a1 1 0 0 1 1.005 -.717a11 11 0 0 0 7.531 -2.527l.263 -.225l.096 -.075a.993 .993 0 0 1 .217 -.112l.112 -.034a.97 .97 0 0 1 .119 -.021l.115 -.007zm3.71 7.293a1 1 0 0 0 -1.415 0l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.32 1.497l2 2l.094 .083a1 1 0 0 0 1.32 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z' /></svg>";
                     break;
-                case 3:
+                case 4:
                     icon = "<svg  xmlns='http://www.w3.org/2000/svg'  width='48'  height='48'  viewBox='0 0 24 24'  fill='none'  stroke='currentColor'  stroke-width='2'  stroke-linecap='round'  stroke-linejoin='round'  class='icon icon-tabler icons-tabler-outline icon-tabler-book-2'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z' /><path d='M19 16h-12a2 2 0 0 0 -2 2' /><path d='M9 8h6' /></svg>";
                     break;
-                case 4:
+                case 5:
                     icon = "<svg  xmlns='http://www.w3.org/2000/svg'  width='48'  height='48'  viewBox='0 0 24 24'  fill='none'  stroke='currentColor'  stroke-width='2'  stroke-linecap='round'  stroke-linejoin='round'  class='icon icon-tabler icons-tabler-outline icon-tabler-school'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M22 9l-10 -4l-10 4l10 4l10 -4v6' /><path d='M6 10.6v5.4a6 3 0 0 0 12 0v-5.4' /></svg>";
                     break;
             }
---
+++ nuxt/components/navbar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="h-fit flex flex-col text-lg divide-y-2 border-y-2 border-white text-[1.25rem]">
+        <NuxtLink class="px-4 py-6 flex align-center gap-3" to="/llista_propostes" @click="close()">
+            <UIcon name="i-heroicons-musical-note-16-solid text-[32px]" />
+            <span>Votacions</span>
+        </NuxtLink>
+        <NuxtLink class="px-4 py-6 flex align-center gap-3" to="/els_meus_grups" @click="close()">
+            <UIcon name="i-heroicons-user-group-16-solid text-[32px]" />
+            <span>Els meus grups</span>
+        </NuxtLink>
+        <NuxtLink class="px-4 py-6 flex align-center gap-3" to="/ranking" @click="close()">
+            <UIcon name="i-heroicons-hand-thumb-up-16-solid text-[32px]" />
+            Ranking provisional
+        </NuxtLink>
+        <NuxtLink class="px-4 py-6 flex align-center gap-3" to="https://forms.gle/qKtFKNCgtQjXmpyNA">
+            <UIcon name="i-heroicons-speaker-wave-20-solid text-[32px]" />
+            Feedback
+        </NuxtLink>
+        <NuxtLink class="px-4 py-6 flex align-center gap-3" to="/logout" @click="close()">
+            <span class="material-symbols-outlined text-[32px]">
+                logout
+            </span>
+            Tancar sessió
+        </NuxtLink>
+    </div>
+</template>
+
+<script>
+import { useAppStore } from '@/stores/app';
+
+export default {
+    data() {
+        return {
+        }
+    },
+    methods: {
+        close() {
+            this.$emit('close');
+        }
+    },
+    created() {
+
+    },
+    computed: {
+
+    },
+    setup() {
+        const store = useAppStore();
+        return { store };
+    },
+}
+</script>
+
+<style scoped></style>
