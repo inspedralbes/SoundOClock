bc04f3893bed7e8e33616f90f983ced76b9f514b
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-12 18:55:38 +0200

    refactor: Update socket event to broadcast settings to all connected clients

--- node/index.js
+++ node/index.js
@@ -1143,7 +1143,7 @@ io.on("connection", (socket) => {
         settings = await comManager.getPublicSettings();
         myCache.set("settings", settings, DEFAULT_CACHE_TTL);
       }
-      socket.emit("sendSettings", settings);
+      io.emit("sendSettings", settings);
     } catch (err) {
       socket.emit("getSettingsError", {
         status: "error",
