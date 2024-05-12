8c8650939e0f1a989f60d7c3a970d65c950773d7
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-05-12 20:33:15 +0200

    refactor: Update black_list_crud.vue to set and filter tracks from socket event

--- nuxt/components/admin/black_list_crud.vue
+++ nuxt/components/admin/black_list_crud.vue
@@ -72,7 +72,9 @@ export default {
 
         socket.on('sendBlacklist', (blacklist) => {
             this.tracks = blacklist;
+            console.log(this.tracks);
             this.filteredTracks = blacklist;
+            console.log(this.filteredTracks);
             this.loading = false;
         });
 
