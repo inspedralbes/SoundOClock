538e007ed62368d2b6e04409194a8d641fd3cc88
Dasbits <a20davsalsos@inspedralbes.cat>
2024-05-23 13:10:49 +0200

    afegir-grup.vue casi acabat falta enllaçar amb la api

--- nuxt/pages/afegir-grup.vue
+++ nuxt/pages/afegir-grup.vue
@@ -1,13 +1,83 @@
 <template>
-    <div>
-        <div v-if="loading" class="loading">
-            <Loader />
-        </div>
-        <div v-else>
 
-            <button @click="console.log(this.user)">asd</button>
+    <div v-if="loading" class="loading">
+        <Loader />
+    </div>
+    <div v-else class="flex flex-col w-full h-full">
+
+        <h1 class="w-full h-fit m-5 text-center text-4xl cursor-default">Afegir-te/treure't de grups </h1>
+
+        <div class="flex flex-row w-full h-full">
+            <div class="w-full h-full">
+                <div class="w-[80%] h-full pl-20">
+                    <div class="flex flex-col justify-center mb-5">
+                        <label for="grup">Grup:</label>
+                        <USelectMenu v-model="selectedCategoryId" :options="categories" option-attribute="abbreviation"
+                            value-attribute="id" placeholder="-- Escull el teu grup --" />
+                    </div>
+                    <div class="flex h-full flex-col">
+                        <label for="curs">Curs:</label>
+                        <ul class="h-[60vh] overflow-auto">
+                            <li v-for="(group, index) in availableCourses" :key="group.id + `key`">
+                                <div class="flex flex-row gap-2 items-center mb-2">
+                                    <div class="checkbox-wrapper-39">
+                                        <label>
+                                            <input type="checkbox" :value="group.id" @change="updateSelected(group)"
+                                                :name="group.id + `name`" :id="group.id + `id`"
+                                                :checked="selectedGroups.includes(group)">
+                                            <span class="checkbox"></span>
+                                        </label>
+                                    </div>
+                                    <div class="no-select">{{ group.abbreviation }}</div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full flex flex-col justify-between">
+                <div class="w-full pr-20">
+                    <div class="flex flex-row mb-3 content-center justify-between">
+                        <h3>CURSOS SELECCIONATS</h3>
+                        <button class="hover:font-semibold hover:text-blue-500"
+                            @click="restartSelectedGroups()">Reiniciar</button>
+                    </div>
+                    <div v-if="!selectedGroups.length > 0" class="text-center">Encara no has seleccionat cap curs
+                    </div>
+                    <div v-else class="flex flex-row flex-wrap gap-3">
+                        <div v-for="group in selectedGroups"
+                            class="w-fit px-3 py-2 bg-[#999] grow text-center rounded-full cursor-default">
+                            {{ group.abbreviation }}
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full flex flex-row justify-between pr-20">
+                    <UButton color="red">Cancel·lar canvis</UButton>
+                    <UButton color="green" @click="this.modals.confirmChanges = true">Confirmar canvis
+                    </UButton>
+                </div>
+            </div>
         </div>
     </div>
+
+    <UModal v-model="this.modals.confirmChanges">
+        <div>
+            <UAlert title="Estàs segur/a que vols aplicar els canvis?" icon="i-heroicons-exclamation-triangle-16-solid"
+                color="orange" variant="subtle" class="p-6">
+                <template #title="{ title }">
+                    <span v-html="title" />
+                </template>
+                <template #description>
+                    <div class="mt-2 flex justify-between gap-2">
+                        <UButton size="md" color="red" @click="modals.confirmChanges = false">Enrere</UButton>
+                        <UButton size="md" color="primary" @click="modals.confirmChanges = false; console.log('selectedGroups: ',selectedGroups)">Continuar</UButton>
+                    </div>
+                </template>
+            </UAlert>
+        </div>
+
+    </UModal>
 </template>
 
 <script>
@@ -23,28 +93,25 @@ export default {
             categories: [],
             selectedCategoryId: '',
             storeGroupsLoading: false,
-            selectedGroups: null,
+            selectedGroups: [],
+            modals: {
+                confirmChanges: false,
+            },
         }
     },
 
     mounted() {
-        if (this.user.role_id < 4) {
-            // User is a not a student (teacher, admin, etc.)
-            comManager.getAllGroupsAndCategories().then((data) => {
-                this.groups = data.allGroups;
-                this.categories = data.allCategories;
-                this.selectedGroups = [];
-                this.loading = false;
+
+        comManager.getAllGroupsAndCategories().then((data) => {
+            this.groups = data.allGroups;
+            this.categories = data.allCategories;
+            this.user.groups.forEach((group) => {
+                this.selectedGroups.push(this.groups.find((grup) => grup.id === group));
             });
-        } else {
-            // User is a student
-            comManager.getPublicGroupsAndCategories().then((data) => {
-                this.groups = data.publicGroups;
-                this.categories = data.publicCategories;
-                this.selectedGroups = '';
-                this.loading = false;
-            });
-        }
+            console.log('selectedGroups: ', this.selectedGroups);
+            this.loading = false;
+        });
+
     },
     computed: {
         availableCourses() {
@@ -57,6 +124,19 @@ export default {
         }
     },
     methods: {
+        updateSelected(groupUpdate) {
+            if (this.selectedGroups.includes(groupUpdate)) {
+                this.selectedGroups = this.selectedGroups.filter((group) => group !== groupUpdate);
+            } else {
+                this.selectedGroups.push(groupUpdate);
+            }
+        },
+        restartSelectedGroups() {
+            this.selectedGroups = [];
+            this.user.groups.forEach((group) => {
+                this.selectedGroups.push(this.groups.find((grup) => grup.id === group));
+            });
+        },
         formatArray(array) {
             return array.map((item) => {
                 return item.id;
@@ -75,6 +155,60 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.checkbox-wrapper-39 *,
+.checkbox-wrapper-39 *::before,
+.checkbox-wrapper-39 *::after {
+    box-sizing: border-box;
+}
 
+.checkbox-wrapper-39 label {
+    display: block;
+    width: 35px;
+    height: 35px;
+    cursor: pointer;
+}
+
+.checkbox-wrapper-39 input {
+    visibility: hidden;
+    display: none;
+}
+
+.checkbox-wrapper-39 input:checked~.checkbox {
+    transform: rotate(45deg);
+    width: 14px;
+    margin-left: 12px;
+    border-color: #24c78e;
+    border-top-color: transparent;
+    border-left-color: transparent;
+    border-radius: 0;
+}
+
+.checkbox-wrapper-39 .checkbox {
+    display: block;
+    width: inherit;
+    height: inherit;
+    border: 3px solid #434343;
+    border-radius: 6px;
+    transition: all 0.375s;
+}
+
+.no-select {
+    user-select: none;
+    /* Evita que el texto sea seleccionable */
+    -webkit-user-select: none;
+    /* Safari */
+    -moz-user-select: none;
+    /* Firefox */
+    -ms-user-select: none;
+    /* Internet Explorer/Edge */
+}
+
+.fixed-bottom {
+    position: fixed;
+    bottom: 50px;
+    /* Puedes ajustar esta distancia según necesites */
+    left: 95%;
+    transform: translateX(-50%);
+}
 </style>
