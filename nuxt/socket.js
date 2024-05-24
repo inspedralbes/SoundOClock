9a0c5222f8040eb29a992aa1e036fd3678bc0e56
mm333mm <a20miqmorcho@inspedralbes.cat>
2024-05-24 13:34:18 +0200

    hot fix

--- nuxt/socket.js
+++ nuxt/socket.js
@@ -53,7 +53,9 @@ socket.on("connect", () => {
   socket.on("loginData", (id, mail, name, picture, token, groups, roleId, roleName) => {
     console.log("loginData recived" + id + mail + name + picture + token + groups + roleId + roleName);
     pinia.setUser(id, mail, name, picture, token, groups, roleId, roleName);
-    if (pinia.getUser().groups.length <= 0) {
+    if (pinia.getUser().groups.length > 0) {
+      navigateTo({ path: "/llista_propostes" });
+    } else {
       navigateTo({ path: "/escollirGrup" });
     }
     if (pinia.getLoadingLogin()) {
