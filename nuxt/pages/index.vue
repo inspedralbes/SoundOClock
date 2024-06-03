c63d1d33374a37b72ab45a0d3f23c10a2d2cddf7
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-06-03 22:31:26 +0200

    Refactor communicationManager.js to add getSelectedSongsOnLanding function

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,42 @@
 <template>
-    <div class="flex flex-col items-center relative h-screen overflow-hidden w-full">
+    <div>
+        <div class="flex justify-between relative">
+            <div class="w-1/2 h-screen flex flex-col justify-center items-center">
+                <div class="text-left">
+                    <h1 class="text-8xl font-bold futura">Sound</h1>
+                    <h1 class="text-8xl font-bold futura">O'Clock</h1>
+                    <p class="text-3xl">El ritme del teu dia, a les teves mans</p>
+                    <button @click="loginGoogle" class="flex flex-row gap-x-4 bg-white p-4 my-4 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                            <path
+                                d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                                fill="#4285F4" />
+                            <path
+                                d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                                fill="#34A853" />
+                            <path
+                                d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                                fill="#FBBC05" />
+                            <path
+                                d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                                fill="#EA4335" />
+                            <path d="M1 1h22v22H1z" fill="none" />
+                        </svg>
+                        <span class="text-black">Inicia sessió amb Google</span>
+                    </button>
+                </div>
+            </div>
+            <div class="w-1/2 h-screen flex flex-col justify-end items-center absolute bottom-0 right-0">
+                <img src="/img/listening_boy2.png" alt="Vota la cançó" class="w-5/6 drop-shadow-xl text-white">
+            </div>
+            <img src="/img/fondo.png" alt="" class="absolute w-screen h-screen z-[-100]">
+        </div>
+    </div>
+
+
+
+
+    <!-- <div class="flex flex-col items-center relative h-screen overflow-hidden w-full">
         <div class="fixed top-0 left-0 w-full h-full overflow-hidden z-[-1] starfield">
             <Star v-for="(star, index) in stars" :key="index" :top="star.top" :left="star.left" :delay="star.delay" />
         </div>
@@ -38,7 +75,8 @@
         <div class="circle"></div>
         <div class="circle min"></div>
 
-    </div>
+    </div> -->
+
 </template>
 
 <script>
@@ -112,5 +150,9 @@ export default {
     z-index: 98;
     background-color: transparent;
     background: none;
+}
+
+.futura {
+    font-family: 'Futura', sans-serif;
 }
 </style>
