43248c5c1c9288091c917dd2a41d4bd03d722662
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-06-11 18:23:43 +0200

    Refactor sideBarMenu2.vue for improved code readability and maintainability

--- nuxt/components/sideBarMenu2.vue
+++ nuxt/components/sideBarMenu2.vue
@@ -1,38 +1,24 @@
 <template>
-    <div class="ml-2 my-2 w-1/6 fixed left-0 overflow-y-auto flex flex-col justify-between"
+    <div class="ml-2 pr-2 my-2 w-1/6 fixed left-0 overflow-y-auto flex flex-col justify-between border-e border-gray-500"
         style="height: calc(100vh - 1rem);">
         <div>
-
-            <img src="/img/logo.jpg" alt="Logo" class="mb-4 w-full" />
+            <div class="mb-4 w-full bg-white rounded-md">
+                <img src="/img/logo.jpg" alt="Logo" class="p-2 w-2/3" />
+            </div>
             <UAccordion :items="accordion" multiple color="sky" variant="solid" size="xl">
                 <template #usuaris>
                     <div class="mx-4">
-                        <UVerticalNavigation :links="sidebar.usuaris_links" :ui="{
-                label: 'text-base',
-                wrapper: 'border-s border-gray-200 dark:border-gray-500 space-y-2',
-                base: 'group block border-s -ms-px leading-6 before:hidden',
-                padding: 'p-0 ps-4',
-                rounded: '',
-                font: '',
-                ring: '',
-                active: 'text-primary-500 dark:text-primary-400 border-current font-semibold text-left',
-                inactive: 'border-transparent hover:border-gray-400 dark:hover:border-sky-500 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300 text-left'
-            }" />
+                        <UVerticalNavigation :links="sidebar.usuaris_links" :ui="vNavUI" />
                     </div>
                 </template>
                 <template #songs>
                     <div class="mx-4">
-                        <UVerticalNavigation :links="sidebar.songs_links" :ui="{
-                label: 'text-base',
-                wrapper: 'border-s border-gray-200 dark:border-gray-500 space-y-2',
-                base: 'group block border-s -ms-px leading-6 before:hidden',
-                padding: 'p-0 ps-4',
-                rounded: '',
-                font: '',
-                ring: '',
-                active: 'text-primary-500 dark:text-primary-400 border-current font-semibold text-left',
-                inactive: 'border-transparent hover:border-gray-400 dark:hover:border-sky-500 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300 text-left'
-            }" />
+                        <UVerticalNavigation :links="sidebar.songs_links" :ui="vNavUI" />
+                    </div>
+                </template>
+                <template #other>
+                    <div class="mx-4">
+                        <UVerticalNavigation :links="sidebar.other_links" :ui="vNavUI" />
                     </div>
                 </template>
             </UAccordion>
@@ -40,9 +26,9 @@
         <div class="gap-y-4">
             <UButton icon="i-heroicons-cog-6-tooth" class="w-full mb-1.5" size="xl" color="sky" variant="solid"
                 label="Configuració" :trailing="false" />
-            <UButton icon="i-heroicons-backward" class="w-full mb-1.5" size="xl" color="sky" variant="ghost"
+            <UButton icon="i-heroicons-backward-solid" class="w-full mb-1.5" size="xl" color="sky" variant="ghost"
                 label="Tornar a Inici" :trailing="false" />
-            <UButton icon="i-heroicons-arrow-left-start-on-rectangle" class="w-full mb-1.5" size="xl" color="sky"
+            <UButton icon="i-heroicons-arrow-left-start-on-rectangle-solid" class="w-full mb-1.5" size="xl" color="sky"
                 variant="ghost" label="Sortir" :trailing="false" />
         </div>
     </div>
@@ -59,7 +45,7 @@ export default {
                 usuaris_links: [
                     {
                         label: 'Usuaris',
-                        // to: '/getting-started'
+                        to: '/admin2'
                     },
                     {
                         label: 'Rols i permisos',
@@ -83,7 +69,17 @@ export default {
                         label: 'Programació de Cançons',
                         // to: '/getting-started'
                     },
+                    {
+                        label: 'Gestió de Megafonia',
+                        // to: '/getting-started'
+                    },
                 ],
+                other_links: [
+                    {
+                        label: 'Gestió de Grups',
+                        // to: '/getting-started/installation'
+                    },
+                ]
             },
             accordion: [
                 {
@@ -96,9 +92,26 @@ export default {
                     label: `Gestió de cançons`,
                     icon: 'i-heroicons-musical-note',
                     slot: 'songs',
-                    defaultOpen: false,
+                    defaultOpen: true,
+                },
+                {
+                    label: `Altres gestions`,
+                    icon: 'i-heroicons-ellipsis-vertical',
+                    slot: 'other',
+                    defaultOpen: true,
                 },
             ],
+            vNavUI: {
+                label: 'text-base',
+                wrapper: 'border-s border-gray-200 dark:border-gray-500 space-y-2',
+                base: 'group block border-s -ms-px leading-6 before:hidden',
+                padding: 'p-0 ps-4',
+                rounded: '',
+                font: '',
+                ring: '',
+                active: 'text-primary-500 dark:text-white border-sky-500 text-left',
+                inactive: 'border-transparent hover:border-gray-400 dark:hover:border-sky-500 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300 text-left'
+            }
         };
     }
 }
