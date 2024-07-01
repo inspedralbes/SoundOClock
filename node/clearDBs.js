0c39167dce4f301eb390d718d0368a5e76f2c27c
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-07-01 11:28:47 +0200

    chore: Update dotenv configuration in clearDBs.js

--- node/clearDBs.js
+++ node/clearDBs.js
@@ -2,7 +2,7 @@ import dotenv, { config } from "dotenv";
 import sqlDB_Manager from "./sqlDB.manager";
 import mongoDBManager from "./mongoDB.manager";
 
-config.dotenv();
+dotenv.config();
 
 async function clearDBs() {
   await mongoDBManager.vaciarAllCollections();
