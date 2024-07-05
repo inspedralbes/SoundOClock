3b86f6aabb3314f1bb33660d6ee0e04c8bf0d23f
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-07-05 09:32:44 +0200

    chore: Update socket path for server
    The socket path for the server was updated to "/socket". This change ensures that the socket connection is established correctly and improves the overall functionality of the application.

--- node/index.js
+++ node/index.js
@@ -38,7 +38,9 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
+  path: "/socket",
 });
+
 const port = 8080;
 const mongoUser = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
@@ -568,9 +570,7 @@ io.on("connection", (socket) => {
     comManager
       .googleLogin(userToken)
       .then((userData) => {
-        // console.log("UserData:", userData);
         let groups = [];
-        console.log(userData);
         // Populate groups array with group_id
         userData.user.groups.forEach((group) => {
           groups.push(group.pivot.group_id);
