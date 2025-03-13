fdfc17f3f48f1d6d0b2e3eaca97f7a31fe347f8c
respinosa <raul.espinosa.go@gmail.com>
2025-03-13 11:25:27 +0100

    Update login environment check from DEVELOPMENT to preprod

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -135,7 +135,7 @@ export default {
     },
     methods: {
         handleLogin() {
-            if (this.$config.public.ENV === 'DEVELOPMENT') {
+            if (this.$config.public.ENV === 'preprod') {
                 socket.emit('publicLogin')
                 this.loading = true;
             } else {
